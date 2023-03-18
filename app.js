c0dcacc77f118ec97dcafc3c660b882a13d4d0e6
ekincare <ekincare@APPLEs-MacBook-Pro.local>
2023-03-18 18:59:47 +0530

    path fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let defferedPrompt;
 if ('serviceWorker' in navigator) {
    navigator.serviceWorker
-      .register('/sw.js') // register also take in the second parameter as object in which we can specify the scope of the service worker e.g ('/sw.js', {scope: '/help/})
+      .register('sw.js') // register also take in the second parameter as object in which we can specify the scope of the service worker e.g ('/sw.js', {scope: '/help/})
       .then(() => {
          console.log("Service Worker Registered");
       })
